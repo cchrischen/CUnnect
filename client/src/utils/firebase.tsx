5082148c332b2803bdb4a662613dfbce9703157c
Chris Chen <cchen6479@gmail.com>
2023-12-13 17:50:22 -0500

    changed firebase

--- client/src/utils/firebase.tsx
+++ client/src/utils/firebase.tsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyAC9pQQ2s1hCPGgOPK3TqnKFqDtMBzdaM8",
-  authDomain: "final-project-c8992.firebaseapp.com",
-  projectId: "final-project-c8992",
-  storageBucket: "final-project-c8992.appspot.com",
-  messagingSenderId: "623878779988",
-  appId: "1:623878779988:web:714b3275ea1677b96f29c1",
+  apiKey: "AIzaSyCk-gl95H9PCpHQ8KsLS7A_3mqLEqQo1X8",
+  authDomain: "cunnect-861bb.firebaseapp.com",
+  projectId: "cunnect-861bb",
+  storageBucket: "cunnect-861bb.appspot.com",
+  messagingSenderId: "1005651194776",
+  appId: "1:1005651194776:web:c75df66995782c05b0b1ed"
 };
 
 const app = initializeApp(config);
